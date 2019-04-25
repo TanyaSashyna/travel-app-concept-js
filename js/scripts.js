4256c472423b1089c3beae919f8ff53b3c5e80a0
Tanya Sashyna <sashina51@gmail.com>
2019-04-25 13:11:51 +0300

    create selects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-let bodyElem = document.body;
 let createElem = tagName => document.createElement ( tagName );
+let defineElem = (nameElem , nameClass) => customElements.define ( nameElem, nameClass );
 
 let wrapper = createElem('div');
 wrapper.style = `
@@ -11,59 +11,110 @@ wrapper.style = `
     border: 1px solid #ccc;
     background: #f5f5f5;
 `;
-bodyElem.appendChild(wrapper);
+document.body.appendChild(wrapper);
 
 
-let h2Tag = createElem('h2');
-h2Tag.textContent = 'Пассажирские перевозки';
-h2Tag.style = `
-    font-size: 25px;
-    font-family: 'Roboto', sans-serif;
-    color: #656565;
-    font-weight: 700;
-    text-align: center;
-`;
-wrapper.appendChild(h2Tag);
-
-
-let pTag = createElem('p');
-pTag.textContent = 'Автобусные пассажирские перевозки нашим комфортабельным автобусом доставят вам удовольствие от поездки. ';
-pTag.style = `
-    padding: 25px 0
-    font-size: 17px;
-    font-family: 'Roboto', sans-serif;
-    color: #929191;
-    text-align: justify;
-`;
-wrapper.appendChild(pTag);
-
-
-class WrapperElem extends HTMLElement {
+class CaptionElem extends HTMLElement {
     constructor() {
         super ();
 
-        let wrapper = createElem('div');
-        //wrapper.className = 'wrapper';
-        wrapper.textContent = 'Lorem ipsum';
+        let caption = createElem('div');
+        caption.className = 'caption-wrap';
+
+        let h2Tag = createElem('h2');
+        h2Tag.textContent = 'Пассажирские перевозки';
+        h2Tag.className = 'caption';
+
+        let pTag = createElem('p');
+        pTag.textContent = 'Автобусные пассажирские перевозки нашим комфортабельным автобусом доставят вам удовольствие от поездки. ';
+        pTag.className = 'legend';
 
         let style = createElem('style');
 
         style.textContent = `
-            .wrapper {
-                padding: 20px;
-                border: 1px solid #ccc;
-                background: #ddddee;
+            .caption-wrap {
+                margin-bottom: 30px;
+            }
+            .caption {
+                font-size: 25px;
+                font-family: 'Roboto', sans-serif;
+                color: #656565;
+                font-weight: 700;
+                text-align: center;
+            }
+            .legend {
+                padding: 25px 0
+                font-size: 17px;
+                font-family: 'Roboto', sans-serif;
+                color: #929191;
+                text-align: justify;
             }
         `;
 
+        caption.appendChild(h2Tag);
+        caption.appendChild(pTag);
+
         this.shadow = this.attachShadow ( { mode: 'open' } );
         this.shadow.appendChild ( style );
-        this.shadow.appendChild ( wrapper );
+        this.shadow.appendChild ( caption );
     }
 }
 
-customElements.define ( 'wrapper-elem', WrapperElem );
+defineElem('caption-elem', CaptionElem);
 
-document.body.appendChild(
-    createElem('wrapper-elem')
-);
+wrapper.appendChild(
+    createElem('caption-elem')
+).style = `display: block`;
+
+
+class SelectionElem extends HTMLElement {
+    constructor(){
+        super();
+
+        const optionsWayFrom = ['Город отправления' , 'Харьков' , 'Железный Порт' , 'Скадовск' , 'Лазурное' , 'Одесса - Затока'];
+        const optionsWayTo = ['Выбрать пункт прибытия', 'Харьков' , 'Железный Порт' , 'Скадовск' , 'Лазурное' , 'Одесса - Затока'];
+
+        let wayFrom = createElem('div');
+        wayFrom.className = 'way-from';
+
+        let wayTo = createElem('div');
+        wayTo.className = 'way-to';
+
+        function createSelect(options , id , parentTag) {
+            let sel = createElem('select');
+            sel.id = id;
+
+            parentTag === wayTo ? sel.style.display = 'none' : null;
+
+            options.forEach ( val => {
+                var opt = createElem( 'option' );
+                opt.innerHTML = val;
+                opt.value = val;
+                sel.appendChild ( opt )
+            });
+
+            parentTag.appendChild( sel );
+            return  parentTag
+        }
+
+        this.shadow = this.attachShadow ( { mode: 'open' } );
+
+        this.shadow.appendChild ( createSelect(optionsWayFrom , 'way_from' , wayFrom) );
+        this.shadow.appendChild ( createSelect(optionsWayTo , 'way_to', wayTo) );
+    }
+}
+
+defineElem('selection-elem', SelectionElem);
+
+wrapper.appendChild(
+    createElem('selection-elem')
+).style = `display: block`;
+
+
+let selectWayFrom = document.querySelector('selection-elem').shadowRoot.getElementById('way_from');
+
+let checkValue = function (e) {
+    console.log(e.target.value)
+};
+
+selectWayFrom.addEventListener('change' , checkValue);
