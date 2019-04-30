6f5c75abc8e2e3c87af18119057e9c4701bd8d13
Tanya Sashyna <sashina51@gmail.com>
2019-04-30 18:10:30 +0300

    Event listener reworked. The tag selects

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@
     let valOne;
 
     //Подумать над решением выбора города.
-    // Т.е если выбран "Х", то в селекте куда будут все пункты кроме "Х".
+    // Т.е если выбран "Х", то в селекте ""куда" будут все пункты кроме "Х".
     //Если это не "Х", то в селекте куда будет только пункт "Х".
-    //Плюс подумать над появлением блока "куда"(должен добавляться на страницу только после выбора города "откуда")
+    //Плюс подумать над появлением блока "куда"(должен появляться на странице только после выбора города "откуда")
     const options = ['Выберите город' , 'Харьков' , 'Железный Порт' , 'Скадовск' , 'Лазурное' , 'Одесса - Затока'];
 
     function createCaption () {
@@ -40,22 +40,10 @@
         createElem(wrapper , 'about-way').style = `display: block`;
     }
 
-    function getValSelectOne (e) {
-        return valOne = e.target.value;
-    }
-
-    function checkValue (e) {
-        console.log(valOne);
-        console.log(e.target.value);
-        createClassAboutWay ()
-    }
-
-    function createClassSelectionElem( textLabel , nameSelect , optionsArr = ['Выберите город'] , checkValue = function(){} , nameTag ) {
+    function createClassSelectionElem( textLabel , nameSelect , checkValue = function(){} , nameTag ) {
         class SelectionElem extends HTMLElement {
             constructor() {
                 super ();
-
-                const options = optionsArr;
                 this.shadow = this.attachShadow ( { mode: 'open' } );
 
                 let caption = document.querySelector ( "#selection" );
@@ -67,22 +55,50 @@
                 let select = this.shadow.querySelector('select');
                 select.name = nameSelect;
 
-                options.forEach ( val => {
-                    let opt = document.createElement( 'option' );
-                    opt.innerHTML = val;
-                    opt.value = val;
-                    select.appendChild ( opt )
-                });
-
                 select.addEventListener('change' , checkValue);
             }
         }
         defineElem(nameTag, SelectionElem);
-        createElem(wrapper , nameTag);
+        createElem(wrapper , nameTag).style.display = nameSelect === 'way-to' ? 'none' : 'block';
+    }
+
+    function addOptions(options , sel) {
+        options.forEach ( val => {
+            let opt = document.createElement( 'option' );
+            opt.innerHTML = val;
+            opt.value = val;
+            sel.appendChild ( opt )
+        })
+    }
+
+    function checkValue (e) {
+        console.log(valOne);
+        console.log(e.target.value);
+
+
+        //createClassAboutWay ()
+    }
+
+    function checkCity (city) {
+        let arr;
+        return arr = city === 'Харьков' ? options.filter ( x => x !== 'Харьков' ) : ['Выберите город' , 'Харьков'];
+    }
+
+    function getValSelectOne (e) {
+        valOne = e.target.value;
+
+        document.querySelector('selection-to').shadowRoot.querySelector('select').innerHTML = '';
+
+        let newOptions = checkCity(e.target.value);
+
+        addOptions(newOptions , document.querySelector('selection-to').shadowRoot.querySelector('select') );
+
+        document.querySelector('selection-to').style.display = 'block'
     }
 
     createCaption ();
-    createClassSelectionElem( 'Откуда' , 'way-from' , options , getValSelectOne  , 'selection-from' );
-    createClassSelectionElem( 'Куда' , 'way-to' , options ,checkValue  , 'selection-to' );
+    createClassSelectionElem( 'Откуда' , 'way-from' , getValSelectOne  , 'selection-from' );
+    addOptions( options , document.querySelector('selection-from').shadowRoot.querySelector('select') );
 
+    createClassSelectionElem( 'Куда' , 'way-to' ,checkValue  , 'selection-to' );
 })();
