f6406ec0b09c0e4ed035ba7e4dc07cdd8590b28a
Tanya Sashyna <sashina51@gmail.com>
2019-05-02 17:26:02 +0300

    createClassAboutWay

--- js/scripts.js
+++ js/scripts.js
@@ -25,24 +25,6 @@
         createElem(wrapper , 'caption-elem').style = `display: block`;
     }
 
-    function createClassAboutWay (headCity) {
-        class AboutWayElem extends HTMLElement {
-            constructor () {
-                super();
-                this.shadow = this.attachShadow ( { mode: 'open' } );
-
-                let aboutWay = document.querySelector ( "#about_way" );
-                this.shadow.appendChild ( aboutWay.content.cloneNode ( true ) )
-
-                /*let wayHead = this.shadow.querySelector('.way-head');
-                wayHead.textContent = headCity;*/
-            }
-        }
-
-        defineElem('about-way', AboutWayElem);
-        createElem(wrapper , 'about-way').style = `display: block`;
-    }
-
     function createClassSelectionElem( textLabel , nameSelect , checkValue = function(){} , nameTag ) {
         class SelectionElem extends HTMLElement {
             constructor() {
@@ -64,6 +46,40 @@
         defineElem(nameTag, SelectionElem);
         createElem(wrapper , nameTag).style.display = nameSelect === 'way-to' ? 'none' : 'block';
     }
+    //--------------------------------------------------------------------------
+
+
+    function createClassAboutWay () {
+        let btnBuy;
+
+        return class AboutWayElem extends HTMLElement {
+            constructor () {
+                super();
+                this.shadow = this.attachShadow ( { mode: 'open' } );
+                let aboutWay = document.querySelector ( "#about_way" );
+                this.shadow.appendChild ( aboutWay.content.cloneNode ( true ) )
+
+                btnBuy = this.shadow.querySelector('.buy-btn');
+                btnBuy.addEventListener('click' , createOrderSheet)
+            }
+            connectedCallback() {
+                console.log(
+                    this.shadow.querySelector('.way-head') ,
+                    this.shadow.querySelector('.picture').src ,
+                    this.shadow.querySelector('.when') , 
+                    this.shadow.querySelector('.time') , 
+                    this.shadow.querySelector('.where') ,
+                    this.shadow.querySelector('.city-time') ,
+                    this.shadow.querySelector('.price') ,
+                )
+            }
+            disconnectedCallback() {
+                console.log('I am removed now');
+                btnBuy.removeEventListener('click' , createOrderSheet)
+            }
+        }
+    }
+    defineElem('about-way', createClassAboutWay());
 
     function addOptions(options , sel) {
         options.forEach ( val => {
@@ -74,25 +90,12 @@
         })
     }
 
-    function checkValue (e) {
-        console.log(valOne);
-        console.log(e.target.value);
-
-        //document.querySelector('about-way') ? document.querySelector('about-way').remove() : null;
-
-        //createClassAboutWay()
-        //Подумать над удалением и вставкой повторяющегося template
-        /*let cap = document.querySelector ( "#caption" );
-        document.querySelector('.wrapper').appendChild(
-            cap.content
-        )*/
-    }
-
     function checkCity (city) {
         let arr;
         return arr = city === 'Харьков' ? options.filter ( x => x !== 'Харьков' ) : ['Выберите город' , 'Харьков'];
     }
 
+    //EventListener
     function getValSelectOne (e) {
         valOne = e.target.value;
 
@@ -105,6 +108,22 @@
         document.querySelector('selection-to').style.display = 'block'
     }
 
+    //EventListener
+    function checkValue (e) {
+        console.log(valOne);
+        console.log(e.target.value);
+
+        document.querySelector('about-way') ? document.querySelector('about-way').remove() : null;
+
+        createElem(wrapper , 'about-way').style = `display: block`;
+    }
+
+    //EventListener
+    function createOrderSheet(e) {
+            console.log('createOrderSheet')
+    }
+
+    //--------------------------------------------------------------------------------------------------
     createCaption ();
     createClassSelectionElem( 'Откуда' , 'way-from' , getValSelectOne  , 'selection-from' );
     addOptions( options , document.querySelector('selection-from').shadowRoot.querySelector('select') );
