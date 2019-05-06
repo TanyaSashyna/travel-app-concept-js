ea7673094e9f33abdcb0742e5fcb4f00848c81f2
Tanya Sashyna <sashina51@gmail.com>
2019-05-06 21:45:22 +0300

    rework class selectionElem

--- js/components/select.js
+++ js/components/select.js
@@ -1,37 +1,10 @@
-(function () {
-    function createClassSelectionElem(textLabel, checkValue = function (){}) {
-        let select;
-
-        return class SelectionElem extends HTMLElement {
-            constructor() {
-                super();
-                this.shadow = this.attachShadow({mode: 'open'});
-
-                let caption = document.querySelector("#selection");
-                this.shadow.appendChild(caption.content.cloneNode(true));
-
-                let label = this.shadow.querySelector('label');
-                label.textContent = textLabel;
-
-                select = this.shadow.querySelector('select');
-                select.addEventListener('change', checkValue);
-            }
-
-            disconnectedCallback() {
-                select.removeEventListener(checkValue);
-                console.log('I am removed checkValue now')
-            }
-        }
-    }
-
-    defineElem('selection-from', createClassSelectionElem('Откуда', getValSelectOne));
-    defineElem('selection-to', createClassSelectionElem('Куда', checkValue));
+function createClassSelectionElem(textLabel) {
+    let select;
 
     function getValSelectOne(e) {
         valOne = e.target.value;
 
         document.querySelector('selection-to').shadowRoot.querySelector('select').innerHTML = '';
-
         let newOptions = methodsLib.checkCity(e.target.value, 'Харьков');
 
         methodsLib.addOptions(
@@ -46,21 +19,53 @@
         console.log(valOne);
         console.log(e.target.value);
 
-        document.querySelector('about-way') ? document.querySelector('about-way').remove() : null;
-        document.querySelector('form-order') ? document.querySelector('form-order').remove() : null;
+        document.querySelector('about-way') ?
+            document.querySelector('about-way').remove() : null;
 
-        createElem(routes, 'about-way').style = `display: block`;
+        document.querySelector('form-order') ?
+            document.querySelector('form-order').remove() : null;
+
+        createElem(routes, 'about-way').style.display = 'block';
     }
 
-    createElem(fromTo, 'selection-from').style.width = '50%';
-    methodsLib.addOptions(
-        options,
-        document.querySelector('selection-from').shadowRoot.querySelector('select')
-    );
+    return class SelectionElem extends HTMLElement {
+        constructor() {
+            super();
+            this.shadow = this.attachShadow({mode: 'open'});
 
-    createElem(fromTo, 'selection-to')
-        .style = `
+            let caption = document.querySelector("#selection");
+            this.shadow.appendChild(caption.content.cloneNode(true));
+
+            let label = this.shadow.querySelector('label');
+            label.textContent = textLabel;
+
+            select = this.shadow.querySelector('select');
+            select.addEventListener(
+                'change',
+                textLabel === 'Откуда' ? getValSelectOne : checkValue
+            );
+        }
+
+        disconnectedCallback() {
+            select.removeEventListener(
+                textLabel === 'Откуда' ? getValSelectOne : checkValue
+            );
+            console.log('I am removed checkValue now')
+        }
+    }
+}
+
+defineElem('selection-from', createClassSelectionElem('Откуда'));
+defineElem('selection-to', createClassSelectionElem('Куда'));
+
+createElem(fromTo, 'selection-from').style.width = '50%';
+methodsLib.addOptions(
+    options,
+    document.querySelector('selection-from').shadowRoot.querySelector('select')
+);
+
+createElem(fromTo, 'selection-to')
+    .style = `
         display: none;
         width: 50%;
     `;
-})();
